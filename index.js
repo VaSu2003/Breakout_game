b4f1304549826651b533ace63aa0fe14b27c1f87
Vaibhav <vaibhav.shukla98871174@gmail.com>
2023-07-26 13:51:36 +0530

    get the array of all block collision and remove the hit blocks

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('#score')
 
 const blockWidth = 100
 const blockHeight = 20
@@ -8,6 +9,9 @@ let currentPosition = userStart;
 
 const ballStart = [270,30]
 let ballCurrentPos = ballStart;
+
+let xDirection = 2
+let yDirection = 2
 
 
 //create block 
@@ -109,4 +113,76 @@ grid.appendChild(ball)
 function drawBall(){
     ball.style.left = ballCurrentPos[0] + 'px'
     ball.style.bottom = ballCurrentPos[1] + 'px'
+}
+
+//Move the ball
+
+function moveBall(){
+    ballCurrentPos[0] += xDirection
+    ballCurrentPos[1] += yDirection
+    drawBall();
+    checkForCollisions();
+}
+
+let timerId = setInterval(moveBall,30)
+
+//check for collisions
+
+function checkForCollisions(){
+
+    //check for block collisions
+    for(let i = 0 ; i< blocks.length; i++){
+        if(
+            (ballCurrentPos[0] > blocks[i].bottomLeft[0] && ballCurrentPos[0] < blocks[i].bottomRight[0]) && ((ballCurrentPos[1] + 20) > blocks[i].bottomLeft[1] && ballCurrentPos[1] < blocks[i].topLeft[1])
+        ){
+            const allBlocks = Array.from(document.querySelectorAll('.block'))
+            // console.log(allBlocks)
+            allBlocks[i].classList.remove('block')
+            blocks.splice(i,1)
+            changeDirection();
+        }
+    }
+
+    //check for wall collisions
+    if(ballCurrentPos[0] >= 540){
+        changeDirection();
+    }
+    if(ballCurrentPos[1] >= 280 ){
+        changeDirection();
+    }
+    if(ballCurrentPos[0] <= 0){
+        changeDirection();
+    }
+    
+
+    // check for game over
+    if(ballCurrentPos[1] <= 0 ){
+        clearInterval(timerId)
+        scoreDisplay.innerHTML = 'You Lose'
+        document.removeEventListener('keydown',moveUser)
+    }
+}
+
+
+function changeDirection(){
+    if(xDirection === 2 && yDirection === 2){
+        yDirection = -2
+        // xDirection = 2
+        return
+    }
+    if(xDirection === 2 && yDirection === -2){
+        // yDirection = -2
+        xDirection = -2 
+        return 
+    }
+    if(xDirection === -2 && yDirection === -2){
+        yDirection = 2
+        // xDirection = 2
+        return 
+    }
+    if(xDirection === -2 && yDirection === 2){
+        xDirection = 2
+        return
+    }
+    
 }
